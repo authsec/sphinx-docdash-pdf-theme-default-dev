1ff10bd619dabef0f5d6009ceafc5c3a14966307
Jens Frey <authsec@coffeecrew.org>
2026-03-12 21:44:03 +0100

    Add admonition styling

--- docdash_pdf_theme/__init__.py
+++ docdash_pdf_theme/__init__.py
@@ -6,7 +6,7 @@ from jinja2 import Environment
 
 logger = logging.getLogger(__name__)
 
-__version__ = "0.1.37"
+__version__ = "0.1.38"
 
 def get_safe_filename(name: str) -> str:
     """Creates a filesystem-safe string from a project name."""
@@ -177,7 +177,7 @@ def config_inited(app, config):
             'content_font_size': r'\normalsize'
         }
 
-        admon_types = ['generic', 'note', 'warning', 'hint', 'danger', 'error', 'caution', 'tip', 'important', 'attention']
+        admon_types = ['generic', 'admonition', 'note', 'warning', 'hint', 'danger', 'error', 'caution', 'tip', 'important', 'attention']
         admon_props = [
             'title_icon', 'title_icon_color', 'title_icon_size', 'title_icon_padding', 'title_decoration_spacing', 
             'title_font', 'title_font_color', 'title_font_size', 
@@ -323,7 +323,7 @@ def setup(app):
             app.add_config_value(f'docdash_{key}', None, 'env')
 
     # Admonition Customization Namespace
-    admon_types = ['generic', 'note', 'warning', 'hint', 'danger', 'error', 'caution', 'tip', 'important', 'attention']
+    admon_types = ['generic', 'admonition', 'note', 'warning', 'hint', 'danger', 'error', 'caution', 'tip', 'important', 'attention']
     admon_props = [
         'title_icon', 'title_icon_color', 'title_icon_size', 'title_icon_padding', 'title_decoration_spacing', 
         'title_font', 'title_font_color', 'title_font_size', 
