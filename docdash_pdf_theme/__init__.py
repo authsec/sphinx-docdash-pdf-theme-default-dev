a9f766bf4735b6607a06705d43ee6150e52bbe78
Jens Frey <authsec@coffeecrew.org>
2026-03-15 23:29:26 +0100

    Fix

--- docdash_pdf_theme/__init__.py
+++ docdash_pdf_theme/__init__.py
@@ -10,7 +10,7 @@ from docutils.parsers.rst import Directive, directives
 
 logger = logging.getLogger(__name__)
 
-__version__ = "0.1.119"
+__version__ = "0.1.121"
 
 def get_safe_filename(name: str) -> str:
     """Creates a filesystem-safe string from a project name."""
