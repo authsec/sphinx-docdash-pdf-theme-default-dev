9a5f2f23e7ebc04498ad89f752504384be5504ad
Jens Frey <authsec@coffeecrew.org>
2026-03-22 19:44:53 +0100

    fix build

--- docdash_pdf_theme/__init__.py
+++ docdash_pdf_theme/__init__.py
@@ -18,7 +18,7 @@ from .utils import (
 
 logger = logging.getLogger(__name__)
 
-__version__ = "0.1.148"
+__version__ = "0.1.149"
 
 # --- DEFAULT CONTAINER TITLE STYLES ---
 DEFAULT_TITLE_STYLES = {
